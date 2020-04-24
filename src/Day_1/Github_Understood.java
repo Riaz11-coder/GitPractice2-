a767bc05e6fa1936734067ed1f52296f80182c9a
Riaz11-coder <64184169+Riaz11-coder@users.noreply.github.com>
2020-04-24 16:38:07 -0400

    Update Github_Understood.java

--- src/Day_1/Github_Understood.java
+++ src/Day_1/Github_Understood.java
@@ -4,5 +4,6 @@ public class Github_Understood {
 
     public static void main(String[] args) {
         System.out.println("I finally get it");
+        System.out.println("And Now Class is over");
     }
 }
