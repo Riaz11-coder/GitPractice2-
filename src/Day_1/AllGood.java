c7acb4fbd7291135f3a2eee01140f133d543226f
Riaz11-coder <64184169+Riaz11-coder@users.noreply.github.com>
2020-04-24 16:30:07 -0400

    Update AllGood.java

--- src/Day_1/AllGood.java
+++ src/Day_1/AllGood.java
@@ -8,5 +8,7 @@ public class AllGood {
         System.out.println("I hope everything is good");
         
         System.out.println("This is the first code I'm writing from Github");
+        
+        Sytem.out.println("This is a very cool feature");
     }
 }
